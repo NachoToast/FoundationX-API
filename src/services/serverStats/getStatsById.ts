15d641b2abeeb32a4ae8963246420dcf643429d8
NachoToast <32235595+NachoToast@users.noreply.github.com>
2025-12-24 15:54:27 +1300

    use externalAPI for server stat fetching

--- src/services/serverStats/getStatsById.ts
+++ src/services/serverStats/getStatsById.ts
@@ -1,6 +1,5 @@
-import { NotFoundError } from '../../errors/NotFoundError.js';
+import { SecondaryRequestError } from '../../errors/SecondaryRequestError.js';
 import { ServerStats } from '../../public/ServerStats.js';
-import { getServerStatsDb } from './db.js';
 
 /**
  * Fetches server stats via SCP: SL server ID.
@@ -9,14 +8,56 @@ import { getServerStatsDb } from './db.js';
  * database.
  */
 export async function getStatsById(serverId: string): Promise<ServerStats> {
-    const server = await getServerStatsDb().findOne({ _id: serverId });
+    const response = await fetch(`https://api.scplist.kr/api/servers/${serverId}`);
 
-    if (server === null) {
-        throw new NotFoundError(
-            'Server Not Found',
-            'Could not find a server with that ID in the database.',
+    if (!response.ok) {
+        throw new SecondaryRequestError(
+            'Server Stats API Request Failure',
+            `The server stats API returned an HTTP ${response.status.toString()} (${response.statusText}) response`,
+            response,
         );
     }
 
-    return server;
+    let parsed: unknown;
+
+    try {
+        parsed = await response.json();
+    } catch (error) {
+        throw new SecondaryRequestError(
+            'Server Stats API Parse Failure',
+            'The server stats API response could not be parsed, the provided server ID may be invalid',
+            error
+        );
+    }
+
+    if (typeof parsed !== 'object' || parsed === null || !('players' in parsed) || typeof parsed.players !== 'string') {
+        throw new SecondaryRequestError(
+            'Server Stats API Parse Failure',
+            'Failed to find player count string in server stats API response',
+            parsed,
+        );
+    }
+
+    let playerCount = 0;
+    let playerCap = 0;
+
+    const [newCount, newCap] = parsed.players.trim().split('/').map(x => {
+        const asNumber = Number(x);
+
+        if (Number.isInteger(asNumber) && asNumber >= 0) {
+            return asNumber;
+        }
+
+        return undefined;
+    });
+
+    if (newCount !== undefined) {
+        playerCount = newCount;
+    }
+
+    if (newCap !== undefined) {
+        playerCap = newCap;
+    }
+
+    return { _id: serverId, playerCap, playerCount, reportedAt: Date.now() }
 }
